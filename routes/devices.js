c88682370e5780c15487ed1e9a4768a9d48634e0
Christian Tschacher <ctschacher@axway.com>
2020-03-08 16:50:12 +0100

    added more routes and rest test

--- routes/devices.js
+++ routes/devices.js
@@ -15,25 +15,72 @@ router.get('/', async (req, res) => {
 })
 
 // getting one
-router.get('/:id', (req, res) => {
+router.get('/:id', getDevice, (req, res) => {
+    res.send(res.device);
     console.log('Received GET request for ID: ' + req.params.id);
-    res.send('This will show ID ' + req.params.id + ' in the future\n');
+    // res.send('This will show ID ' + req.params.id + ' in the future\n');
 })
 
 // Create entry
-router.post('/', (req, res) => {
-    console.log('Received GET request for ID: ' + req.params.id);
+router.post('/', async (req, res) => {
+    const device = new Device({
+        name: req.body.name,
+        project: req.body.project
+    });
+
+    try {
+        const newDevice = await device.save();
+        res.status(201).json(newDevice);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 })
 
 // Update entry
-router.patch('/:id', (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
+router.patch('/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
     console.log('Received GET request for ID: ' + req.params.id);
+    if (req.body.name != null) {
+        res.device.name = req.body.name;
+    }
+    if (req.body.project != null) {
+        res.device.project = req.body.project;
+    }
+
+    // try to update device
+    try {
+        const updatedDevice = await res.device.save();
+        res.json(updatedDevice);
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 // Delete entry
-router.delete('/:id', (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
+router.delete('/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
     console.log('Received GET request for ID: ' + req.params.id);
-})
+    try {
+        await res.device.remove();
+        res.json({ message: 'Deleted device ' + res.device.name});
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+});
+
+async function getDevice(req, res, next) {
+    let device;
+
+    try {
+        device = await Device.findById(req.params.id);
+        if (device == null) {
+            return res.status(404).json({ message: 'Cannot find device in database'})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+    res.device = device;
+    next();
+}
 
 
 
