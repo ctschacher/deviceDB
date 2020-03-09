b4afb3b344c0b3c9d3eaa26d82be5cb52c222d26
Christian Tschacher <ctschacher@axway.com>
2020-03-09 08:04:59 +0100

    Changed API from JSON to HTML and started with HTML page, todo: HTML template

--- routes/devices.js
+++ routes/devices.js
@@ -2,12 +2,60 @@ const express = require('express');
 const router = express.Router();
 const Device = require('../models/devices');
 
+
 // getting all
 router.get('/', async (req, res) => {
     try {
         const devices = await Device.find();
-        res.json(devices);
+        console.log('Received GET request on /');
+        let table = '';
+        table += `  <form action="http://localhost:3000/devices/" method="post">
+                        <td>[ID]</td>
+                        <td><input type="text" id="name" name="name"></input></td>
+                        <td><input type="text" id="project" name="project"></input></td> 
+                        <td><input type="submit" value="ADD" /></form></td>
+                    </form>`;
         
+        for(let i = 0; i < devices.length; i++) {
+            table += `
+            <tr>
+                <td>${devices[i]._id}</td>
+                <td>${devices[i].name}</td>
+                <td>${devices[i].project}</td>
+                <td><form action="http://localhost:3000/devices/delete/${devices[i]._id}" method="post"><input type="submit" value="DELETE" /></form></td>
+            </tr>`;
+        }
+        let htmlPage = `
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Device DB</title>
+                    <style>
+                table, th, td {
+                border: 0.1px solid grey;
+                },
+                body { 
+                    background: url(img/cream_dust.png) repeat 0 0;
+                }
+                </style>
+                </head>
+                <body>
+                <h1>Content of device DB</h1>
+                <table style="width:30%">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Project</th>
+                    <th>Action</th>
+                </tr>
+                ${table}
+                </table> 
+                </body>
+            </html>`;
+        res.send(htmlPage);
+        // res.json(devices);
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
@@ -18,11 +66,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', getDevice, (req, res) => {
     res.send(res.device);
     console.log('Received GET request for ID: ' + req.params.id);
-    // res.send('This will show ID ' + req.params.id + ' in the future\n');
+
 })
 
 // Create entry
 router.post('/', async (req, res) => {
+  
     const device = new Device({
         name: req.body.name,
         project: req.body.project
@@ -30,37 +79,35 @@ router.post('/', async (req, res) => {
 
     try {
         const newDevice = await device.save();
-        res.status(201).json(newDevice);
+        // res.status(201).json(newDevice);
+        res.redirect('/devices');
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 })
 
 // Update entry
-router.patch('/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
+router.post('/update/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
     console.log('Received GET request for ID: ' + req.params.id);
-    if (req.body.name != null) {
-        res.device.name = req.body.name;
-    }
-    if (req.body.project != null) {
-        res.device.project = req.body.project;
-    }
+    if (req.body.name) res.device.name = req.body.name;
+    if (req.body.project) res.device.project = req.body.project;
 
     // try to update device
     try {
         const updatedDevice = await res.device.save();
         res.json(updatedDevice);
+        // res.redirect('/devices');
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 })
 
 // Delete entry
-router.delete('/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
-    console.log('Received GET request for ID: ' + req.params.id);
+router.post('/delete/:id', getDevice, async (req, res) => {   // patch instead (only info that gets passed) of put (which updates all infos at once)
+    console.log('Received POST request to delete ID: ' + req.params.id);
     try {
         await res.device.remove();
-        res.json({ message: 'Deleted device ' + res.device.name});
+        res.redirect('/devices');
     } catch (error) {
         res.status(500).json({ message: error.message});
     }
@@ -71,9 +118,7 @@ async function getDevice(req, res, next) {
 
     try {
         device = await Device.findById(req.params.id);
-        if (device == null) {
-            return res.status(404).json({ message: 'Cannot find device in database'})
-        }
+        if (!device) return res.status(404).json({ message: 'Cannot find device in database' });
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -10,6 +11,8 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database'));
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const deviceDbRouter = require('./routes/devices.js');
 app.use('/devices', deviceDbRouter);
